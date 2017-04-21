0c19cdd96aeb63b0cf794b13a884883ce47f7497
John Stevenson <john@jr0cket.co.uk>
2017-04-21 23:39:10 +0100

    Documented the code so far
    Documented the state, component, render and repl code sections of the
    conference-om/core namespace

--- src/cljs/conference_om/core.cljs
+++ src/cljs/conference_om/core.cljs
@@ -4,7 +4,16 @@
 
 (enable-console-print!)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; State for the application
+
+;; Use defonce to define the app state avoids the state being
+;; reset each time figwheel reloads the code
 (defonce app-state (atom {:text "Hello Chestnut!"}))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Components
 
 (defn root-component [app owner]
   (reify
@@ -12,7 +21,17 @@
     (render [_]
       (dom/div nil (dom/h1 nil (:text app))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Render components with om/root
+
 (om/root
  root-component
  app-state
  {:target (js/document.getElementById "app")})
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Code to test the app
+
+;; Update the state by replacing the string in :text
+(swap! app-state update :text #(str "Hello from the REPL"))
